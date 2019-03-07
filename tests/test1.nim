3a6593e144d58aba02374e019107f79deb8a557d
Clyybber <darkmine956@gmail.com>
2019-03-08 00:20:42 +0100

    Cleanup

--- tests/test1.nim
+++ tests/test1.nim
@@ -29,57 +29,7 @@
 import ../nimage
 import streams
 
-import times, os
-
-template benchmark(code: untyped) =
-  block:
-#    let t0 = epochTime()
-    code
-#    let elapsed = epochTime() - t0
-#    echo "CPU Time: ", $elapsed, "s"
-
 when isMainModule:
-  GC_disable()
-  benchmark:
-    # var buf5 = openFileStream("tests/bttf-gray.png", fmRead)
-    # var img5 = load_png(buf5)
-    # buf5.close()
-    # assert(img5[26, 89] == NColor(0x707070FF))
-    # var out5 = openFileStream("tests/outbttf-gray.png", fmWrite)
-    # let opts5 = new_opts(ColorType.gray)
-    # img5.save_png(out5, opts5)
-    # out5.close()
-    # # Make sure we can read the images we're writing
-    # var buf4 = openFileStream("tests/bttf-gray.png", fmRead)
-    # let img4 = load_png(buf4)
-    # buf4.close()
-    # assert(img4.width == img5.width)
-    # assert(img4.height == img5.height)
-    # for i in 0..img4.height-1:
-    #     for j in 0..img4.width-1:
-    #         assert(img4[i, j] == img5[i, j])
-
-    # var buf3 = openFileStream("tests/bttf-palette.png", fmRead)
-    # let img3 = load_png(buf3)
-    # assert($img3[0, 0] == "010601FF")
-    # buf3.close()
-    # var out3 = openFileStream("tests/outbttf-gray.png", fmWrite)
-    # let opts3 = new_opts(ColorType.graya)
-    # img3.save_png(out3, opts3)
-    # out3.close()
-
-    # var buf2 = openFileStream("tests/bttf.png", fmRead)
-    # let img2 = load_png(buf2)
-    # buf2.close()
-    # var out2 = openFileStream("tests/outbttf.png", fmWrite)
-    # let opts2 = new_opts(ColorType.rgb)
-    # img2.save_png(out2, opts2)
-    # out2.close()
-
     var buf1 = openFileStream("tests/test1.png", fmRead)
     let img1 = load_png(buf1)
-    # assert($img1[0, 0] == "3C3C3CFF")
     buf1.close()
-    # var out1 = openFileStream("tests/outxkcd.png", fmWrite)
-    # img1.save_png(out1)
-    # out1.close()
